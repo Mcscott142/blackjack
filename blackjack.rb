287c0045cb47dcddb155ce559b7b619e74f297d0
Colin Scott <m.colin.scott@gmail.com>
2014-06-03 14:44:03 -0400

    most classes defined, changing scoring

--- blackjack.rb
+++ blackjack.rb
@@ -1,30 +1,74 @@
-class Card
-  def initialize
-    @suit
-    @value
-    @type
+class Card #create card with suit type and value
+  attr_reader :rank, :suit
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def face_card?
+    ['J', 'Q', 'K'].include?(@rank)
+  end
+
+  def value
+    if self.face_card?
+      10
+
+     elsif self.rank =='A'
+      11 #|| 1
+
+    else
+      self.rank.to_i
+    end
   end
 end
 
-class Deck
+
+SUITS = ['♠','♥','♦','♣']
+VALUES = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+
+class Deck #build deck of 52 cards and deal cards from deck
+  attr_reader :deck_of_cards
+
   def initialize
-    @card
-  end
+    @deck_of_cards =[]
+
+    SUITS.each do |suit|
+      VALUES.each do |value|
+         @deck_of_cards << Card.new(value, suit)
+       end
+     end
+
+     @deck_of_cards.shuffle!
+   end
 
   def deal
+    @deck_of_cards.pop
   end
 end
 
-class Hand
-  def initialize
-    @cards
-    @score
+
+class Hand # calculate score of hand, hit, stay, win, push or bust
+  attr_reader :collection
+  def initialize(collection)
+    @collection = collection
   end
 
-  def hit
+  def score
+    # add score here
+    score = 0
+    collection.each do |card|
+     puts card.rank
+     score += card.value
+    end
+    score
   end
-
-  def stand
-  end
-
 end
+
+deck = Deck.new
+
+card_1 = deck.deal
+card_2 = deck.deal
+
+hand = Hand.new([card_1, card_2])
+puts hand.score
+
