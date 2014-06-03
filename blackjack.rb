bb176c29df6775ce77dc34451c646636d806fbc1
Colin Scott <m.colin.scott@gmail.com>
2014-06-03 10:41:34 -0400

    initial shell

---
+++ blackjack.rb
@@ -0,0 +1,30 @@
+class Card
+  def initialize
+    @suit
+    @value
+    @type
+  end
+end
+
+class Deck
+  def initialize
+    @card
+  end
+
+  def deal
+  end
+end
+
+class Hand
+  def initialize
+    @cards
+    @score
+  end
+
+  def hit
+  end
+
+  def stand
+  end
+
+end
